e066a157b06005aa0cfa97feeec55c51800e6cb4
elprofessor67 <jeeshanr599@gmail.com>
2025-10-01 18:44:40 +0530

    Change Agora URL

--- http/agoraHttp.js
+++ http/agoraHttp.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const APP_ID = process.env.NEXT_PUBLIC_AGORA_APPID;
 const TOKEN = process.env.NEXT_PUBLIC_REST_TOKEN;
 
-export const getBroadcastInfoRequest = async (CHANNEL_NAME) =>  axios.get(`https://api.agora.io/dev/v1/channel/user/${APP_ID}/${CHANNEL_NAME}`, {
+export const getBroadcastInfoRequest = async (CHANNEL_NAME) =>  axios.get(`https://webrtc.rafikyconnect.net/dev/v1/channel/user/${APP_ID}/${CHANNEL_NAME}`, {
     headers: {
       'Authorization': `Basic ${TOKEN}`
     }
