891152e31a55037fbd074b6810c5cf56b11444f6
elprofessor67 <jeeshanr599@gmail.com>
2025-10-13 15:35:43 +0530

    fix mobile view

--- components/LanguageSelect.jsx
+++ components/LanguageSelect.jsx
@@ -9,10 +9,10 @@ const LanguageSelect = () => {
 
   return (
     <div className='h-screen w-screen flex flex-col items-center justify-start py-10 px-8 pt-20 overflow-y-auto'>
-      <div className='w-full flex items-center justify-center flex-row gap-4'>
+      <div className='w-full flex items-center justify-center flex-row md:gap-4 gap-2'>
         {
           Array.from({ length: 5 }).map((_, index) => (
-            <img key={index}  src={`/logo/logo-${index + 1}.png`} alt="Livello Logo" className='w-[5rem] md:w-[7rem] object-contain aspect-square' />
+            <img key={index}  src={`/logo/logo-${index + 1}.png`} alt="Livello Logo" className='w-[4rem] md:w-[7rem] object-contain aspect-square' />
           ))
         }
       </div>
--- components/Listner.jsx
+++ components/Listner.jsx
@@ -895,10 +895,10 @@ const Listner = () => {
               height="200"
             /> */}
 
-            <div className='w-full flex items-center justify-center flex-row gap-4 mt-10'>
+            <div className='w-full flex items-center justify-center flex-row md:gap-4 gap-2 mt-10'>
               {
                 Array.from({ length: 5 }).map((_, index) => (
-                  <img key={index} src={`/logo/logo-${index + 1}.png`} alt="Livello Logo" className='w-[5rem] md:w-[7rem] object-contain aspect-square' />
+                  <img key={index} src={`/logo/logo-${index + 1}.png`} alt="Livello Logo" className='w-[4rem] md:w-[7rem] object-contain aspect-square' />
                 ))
               }
             </div>
